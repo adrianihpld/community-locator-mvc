6eb3217230e1cae042c04e9213d73b057d87f702
adrianihpld <139892026+adrianihpld@users.noreply.github.com>
2024-01-13 20:24:37 -0600

    Add project files.

---
+++ Runners/Controllers/ClubController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Runners.Data;
+using Runners.Interfaces;
+using Runners.Models;
+
+
+namespace Runners.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly IClubRepository _clubRepository;
+
+        public ClubController(IClubRepository clubRepository)
+        {
+            _clubRepository = clubRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Club> clubs = await _clubRepository.GetAll();
+            return View(clubs);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            Club club = await _clubRepository.GetByIdAsync(id);
+            return View(club);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Runners/Controllers/RaceController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Runners.Data;
+using Runners.Interfaces;
+using Runners.Models;
+
+namespace Runners.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly IRaceRepository _raceRespository;
+
+        public RaceController(IRaceRepository raceRespository)
+        {
+            _raceRespository = raceRespository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Race> races = await _raceRespository.GetAll();
+            return View(races);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            Race race = await _raceRespository.GetByIdAsync(id);
+            return View(race);
+        }
+    }
+}
